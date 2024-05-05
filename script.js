9c5d99da1b7fd14ecaa487ee530bf79d9daa3c59
MD MOTIUR RAHMAN <163854501+MRPROTIK@users.noreply.github.com>
2024-05-05 11:04:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const questions = [
+    {
+    question: "What is the Largest country in the world?",
+    options: ["Vatikan city", "England", "America", "Russia"],
+    answer: "Russia"},
+
+    {
+    question: "What is the countries with the largest population 2024?",
+    options: ["China", "England", "India", "Russia"],
+    answer: "India"
+    },
+
+    {
+    question: "What is the biggest animal in the world?",
+    options: ["Elephant", "Ziraph", "Blue whale", "Lion"],
+    answer: "Blue whale"
+},
+
+{
+    question: "What is the Largest sea in the world?",
+    options: ["Mediterranean Sea", "Caribbean Sea", "Bering Sea", "The East China Sea"],
+    answer: "Mediterranean Sea"
+},
+
+{
+    question: "What is the Largest bird in the world?",
+    options: ["Greater Rhea", "Cassowary", "Dalmatian Pelican", "Ostrich"],
+    answer: "Ostrich"
+
+},
+
+{
+    question: "What is the Largest tree in the world?",
+    options: ["General Sherman", "General Grant", "President", "King Arthur"],
+    answer: "General Sherman"
+
+},
+
+{
+    question: "What is the Longest beach in the world?",
+    options: ["Padre Island(U.S.A)", "Praia do Cassino(Brazil)", "Cox's Bazar Beach(Bangladesh)", "Ninety Mile Beach(Australia)"],
+    answer: "Praia do Cassino(Brazil)"
+},
+
+{
+    question: "What is the Longest Road Network country in the world?",
+    options: ["China", "India", "U.S.A", "Russia"],
+    answer: "U.S.A"
+},
+
+{
+    question: "What is the Longest River in the world?",
+    options: ["Mississipi River", "Yellow River", "Amazon River", "Nile River"],
+    answer: "Nile River"
+},
+{
+    question: "What is the Largest lake in the world?",
+    options: ["Lake Superior", "Lake Viktoria", "Lake Michigan", "Caspian sea"],
+    answer: "Caspian sea"
+
+}
+];
