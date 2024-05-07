25ca1fcb665cf03331ed0cf363223e78f9454e60
MD MOTIUR RAHMAN <163854501+MRPROTIK@users.noreply.github.com>
2024-05-07 19:51:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
-const questions = [
+var q1 = "What is the Largest country in the world?";
+var q2 = "What is the biggest animal in the world?";
+var q3 = "What is the countries with the largest population 2024?";
+var q4 = "What is the Largest sea in the world?";
+var q5 = "What is the Longest beach in the world?";
+
+
+var question = [q1, q2, q3, q4, q5];
+//var myButton = document.getElementById("button1");
+//myButton.addEventListener("click", question);
+
+
+//function question(){
+
+function askQuestion(question){
+    var answere = prompt(question);
+    return answere;
+}
+// function for run the quize
+function runQuize(){
+    var score = 0;
+    for ( var i = 0; i < question.length; i++){
+        var userAnswer = askQuestion(question[i]);
+        score++
+    }
+    alert("quize comnpleted! Your score is: " + score + " / " + question.length);
+}
+runQuize();
+
+
+
+
+
+
+
+
+
+
+/*const questions = [
     {
     question: "What is the Largest country in the world?",
     options: ["Vatikan city", "England", "America", "Russia"],
@@ -59,4 +97,5 @@ const questions = [
     answer: "Caspian sea"
 
 }
-];
+];*/
+
