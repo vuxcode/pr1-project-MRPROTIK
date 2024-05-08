9d59a6cb78e3ed10cb243d45c867a240ef3871d0
MD MOTIUR RAHMAN <163854501+MRPROTIK@users.noreply.github.com>
2024-05-08 20:01:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,101 +1,41 @@
+
+// Here is our question i set for the user
 var q1 = "What is the Largest country in the world?";
 var q2 = "What is the biggest animal in the world?";
 var q3 = "What is the countries with the largest population 2024?";
 var q4 = "What is the Largest sea in the world?";
 var q5 = "What is the Longest beach in the world?";
 
+// Here is the multiple option for every question
+var o1 = ["A) Russia",  "B) India",  "C) Sweden",  "D) U.S.A"];
+var o2 = ["A) Elephant",  "B) Ziraph",  "C) Blue whale",  "D) Lion"];
+var o3 = ["A) China",  "B) England",  "C) India",  "D) Russia"];
+var o4 = ["A) Mediterranean Sea",  "B) Caribbean Sea",  "C) Bering Sea",  "D) The East China Sea"];
+var o5 = ["A) Padre Island(U.S.A)",  "B) Praia do Cassino(Brazil)",  "C) Cox's Bazar Beach(Bangladesh)", "D) Ninety Mile Beach(Australia)"]
 
+// Here is the correct answer for every question
+
+var a1 = "A) Russia";
+var a2 = "C) Blue whale";
+var a3 = "C) India";
+var a4 = "B) Caribbean Sea";
+var a5 = "B) Praia do Cassino(Brazil)"
+
+// Here i create a array for every question, answere and correct answere
 var question = [q1, q2, q3, q4, q5];
-//var myButton = document.getElementById("button1");
-//myButton.addEventListener("click", question);
-
-
-//function question(){
-
-function askQuestion(question){
-    var answere = prompt(question);
-    return answere;
+var option = [o1, o2, o3, o4, o5];
+var answer = [a1, a2, a3, a4, a5];
+var index = 0;
+function Quize(){
+    var output = document.getElementById("question");
+    // SHOW THE CURRENT QUESTION
+    output.innerHTML = (question[index]);
+    // WHAT HAPPENS NEXT? You wanted to update the index number!!!!!!!!
+    index++;
+    var output = document.getElementById("option");
+    output.innerHTML = (option[index]);
+    index++;
+    var output = document.getElementById("answer");
+    output.innerHTML = (answer[index]);
+    index++;
 }
-// function for run the quize
-function runQuize(){
-    var score = 0;
-    for ( var i = 0; i < question.length; i++){
-        var userAnswer = askQuestion(question[i]);
-        score++
-    }
-    alert("quize comnpleted! Your score is: " + score + " / " + question.length);
-}
-runQuize();
-
-
-
-
-
-
-
-
-
-
-/*const questions = [
-    {
-    question: "What is the Largest country in the world?",
-    options: ["Vatikan city", "England", "America", "Russia"],
-    answer: "Russia"},
-
-    {
-    question: "What is the countries with the largest population 2024?",
-    options: ["China", "England", "India", "Russia"],
-    answer: "India"
-    },
-
-    {
-    question: "What is the biggest animal in the world?",
-    options: ["Elephant", "Ziraph", "Blue whale", "Lion"],
-    answer: "Blue whale"
-},
-
-{
-    question: "What is the Largest sea in the world?",
-    options: ["Mediterranean Sea", "Caribbean Sea", "Bering Sea", "The East China Sea"],
-    answer: "Mediterranean Sea"
-},
-
-{
-    question: "What is the Largest bird in the world?",
-    options: ["Greater Rhea", "Cassowary", "Dalmatian Pelican", "Ostrich"],
-    answer: "Ostrich"
-
-},
-
-{
-    question: "What is the Largest tree in the world?",
-    options: ["General Sherman", "General Grant", "President", "King Arthur"],
-    answer: "General Sherman"
-
-},
-
-{
-    question: "What is the Longest beach in the world?",
-    options: ["Padre Island(U.S.A)", "Praia do Cassino(Brazil)", "Cox's Bazar Beach(Bangladesh)", "Ninety Mile Beach(Australia)"],
-    answer: "Praia do Cassino(Brazil)"
-},
-
-{
-    question: "What is the Longest Road Network country in the world?",
-    options: ["China", "India", "U.S.A", "Russia"],
-    answer: "U.S.A"
-},
-
-{
-    question: "What is the Longest River in the world?",
-    options: ["Mississipi River", "Yellow River", "Amazon River", "Nile River"],
-    answer: "Nile River"
-},
-{
-    question: "What is the Largest lake in the world?",
-    options: ["Lake Superior", "Lake Viktoria", "Lake Michigan", "Caspian sea"],
-    answer: "Caspian sea"
-
-}
-];*/
-
